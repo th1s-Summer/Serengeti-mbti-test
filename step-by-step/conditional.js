2676f8428d0da468a74131d43bd450f45a5497f5
Sebin <93520549+th1s-Summer@users.noreply.github.com>
2022-01-31 11:21:36 +0900

    Update conditional.js

--- step-by-step/conditional.js
+++ step-by-step/conditional.js
@@ -1,6 +1,13 @@
 const age = parseInt(prompt("How old are you?"));
-if (isNaN(age)) {
-    alert("Please write a number");
-} else {
-    alert("Thank you for writing your age");
+
+if (isNaN(age) || age < 0) {
+    alert("Please write a real number");
+} else if (age < 18) {
+    alert("You are too young! You can't drink!");
+} else if (age >= 18 && age <= 50) {
+    alert("You can drink!")
+} else if (age > 50 && age <= 80) {
+    alert("You shold exercise 🚵‍♀️")
+} else if (age > 80) {
+    alert("You can do whatever you want!")
 }
