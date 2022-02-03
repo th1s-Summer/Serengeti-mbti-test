e0a9abfe23210417d1c89f1a05166208a27daa68
Sebin <93520549+th1s-Summer@users.noreply.github.com>
2022-02-03 22:29:21 +0900

    Update app.js

--- step-by-step/app.js
+++ step-by-step/app.js
@@ -1,7 +1,13 @@
 const something = document.getElementById("something");
 
 function clickEvent() {
-    something.style.color = "blue";
-    console.log("something was clicked!")
+    const randomColor = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+    something.style.color = randomColor;
+    console.log("something was clicked!");
 }
 something.addEventListener("click", clickEvent);
+
+function getRandom() {
+    return Math.round(Math.random() * 0xffffff).toString(16);
+}
+console.log(getRandom());
