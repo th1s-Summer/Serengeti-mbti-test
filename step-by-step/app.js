f048d1637e95538d855961f187d147096bee6980
Sebin <93520549+th1s-Summer@users.noreply.github.com>
2022-02-08 22:26:01 +0900

    CSS in Javascript

--- step-by-step/app.js
+++ step-by-step/app.js
@@ -1,34 +1,14 @@
-const something = document.getElementById("something");
+const h1 = document.querySelector("h1");
 
-function clickEvent() {
-    const randomColor = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-    something.style.color = randomColor;
-    console.log("something was clicked!");
+function changeColor() {
+    const currentColor = h1.style.color;
+    let setterColor;
+
+    if (currentColor === "green") {
+        setterColor = "blue";
+    } else {
+        setterColor = "green";
+    }
+    h1.style.color = setterColor;
 }
-
-function mouseEvent() {
-    something.innerText = "boooooo! mouse is here!";
-}
-
-function mouseLeave() {
-    something.innerText = "bye bye- mouse is gone!";
-}
-
-function windowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function widowOffline() {
-    alert("Wher are you? Are you no-WIFI zone? :( ");
-}
-
-function windowOnline() {
-    alert("Yes! We can do everything! :p ");
-}
-
-something.addEventListener("click", clickEvent);
-something.addEventListener("mouseenter", mouseEvent);
-something.addEventListener("mouseleave", mouseLeave);
-window.addEventListener("resize", windowResize);
-window.addEventListener("offline", widowOffline);
-window.addEventListener("online", windowOnline);
+h1.addEventListener("click", changeColor);
