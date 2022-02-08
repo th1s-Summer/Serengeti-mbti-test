ba023016897e7d83cc0610b0beed06ce62964cf6
Sebin <93520549+th1s-Summer@users.noreply.github.com>
2022-02-08 21:45:05 +0900

    add event

--- step-by-step/app.js
+++ step-by-step/app.js
@@ -14,6 +14,21 @@ function mouseLeave() {
     something.innerText = "bye bye- mouse is gone!";
 }
 
+function windowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function widowOffline() {
+    alert("Wher are you? Are you no-WIFI zone? :( ");
+}
+
+function windowOnline() {
+    alert("Yes! We can do everything! :p ");
+}
+
 something.addEventListener("click", clickEvent);
 something.addEventListener("mouseenter", mouseEvent);
 something.addEventListener("mouseleave", mouseLeave);
+window.addEventListener("resize", windowResize);
+window.addEventListener("offline", widowOffline);
+window.addEventListener("online", windowOnline);
