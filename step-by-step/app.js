2a634eedd189ddb59181f6612002944e7fff95ff
Sebin <93520549+th1s-Summer@users.noreply.github.com>
2022-02-16 23:38:00 +0900

    Update app.js

--- step-by-step/app.js
+++ step-by-step/app.js
@@ -1,15 +1,32 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const title = document.querySelector("h1");
+const greeting = document.querySelector("h1");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    title.classList.remove(HIDDEN_CLASSNAME);
-    title.innerHTML = `hello ${username}`;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(someone) {
+    greeting.innerHTML = `hello ${someone}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    //show the loginform
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greetings 
+    paintGreetings(savedUsername);
+
+}
+console.log(savedUsername);
