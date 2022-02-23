328e756dce280051e112ed89192215ba2d10677e
Sebin <93520549+th1s-Summer@users.noreply.github.com>
2022-02-24 08:04:43 +0900

    Create quotes.js

---
+++ js/quotes.js
@@ -0,0 +1,32 @@
+const quotes = [
+    {
+        quote: "그대에게 가장 좋은 휴식은 잠이고, 그대는 자주 잠을 청한다. 하지만 그러면서도 잠자는 것에 지나지 않는 죽음을 무척이나 두려워한다.",
+        author: "멋진 신세계",
+    },
+    {
+        quote: "우리는 천성적으로 더 많은 것을 알고 싶어 합니다. 그래서 계속 배웁니다. 세상에 대한 우리의 지식은 계속 성장합니다. 우리가 배울 수 있는 것은 한계가 있지만 우리는 알고자 하는 욕망을 불태웁니다.",
+        author: "모든 순간의 물리학",
+    },
+    {
+        quote: "지식은 아주 작은 공간의 심오한 구조 속에, 시간의 특성 속에, 블랙홀의 운명 속에, 그리고 우리 생각의 기능 속에 있습니다.",
+        author: "모든 순간의 물리학",
+    },
+    {
+        quote: "어제와 똑같이 살면서 다른 미래를 기대하는 것은 정신병 초기 증상이다",
+        author: "아인슈타인",
+    },
+    {
+        quote: "인생은 곱셈이다. 어떤 찬스가 와도 내가 제로면 아무런 의미가 없다.",
+        author: "나카무라 미츠루",
+    },
+    {
+        quote: "abs",
+        author: "tessr",
+    }
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerText = randomQuote.quote;
+quote.innerText = randomQuote.author;
